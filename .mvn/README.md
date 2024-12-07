7d8f22180eb518be29291782e9f59e42071e26bf
valuetodays <abcdefg20111111@gmail.com>
2024-12-07 12:02:03 +0800

    init

---
+++ .mvn/README.md
@@ -0,0 +1,6 @@
+readme
+
+official page: https://maven.apache.org/configure.html\
+
+- maven.config: additional config for `mvn` command or `mvnw` command
+- jvm.config: additional config for `mvn` command or `mvnw` command
---
+++ docs/scripts-for-drone-ci/ci-publish.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# set variable
+WORKSPACE_PATH=/drone/src
+
+WORKBENCH_DIR=/root/workbench_dir
+WORKBENCH_BAK_DIR=${WORKBENCH_DIR}'/backup'
+WORKBENCH_DEPLOY_DIR=${WORKBENCH_DIR}'/deploy'
+# EDIT HERE
+APP_JAR_NAME=quarkus-demo-runner.jar
+APP_JAR_DIR=${WORKSPACE_PATH}/target
+
+echo start on $(date "+%Y-%m-%d %H:%M:%S")
+shell_directory=$(cd `dirname $0`; pwd)
+echo ${shell_directory}
+echo current_dir=$(pwd)
+
+timeStampStr=$(date "+%Y-%m-%d_%H_%M_%S")
+APP_JAR_BAK=${WORKBENCH_BAK_DIR}'/'${APP_JAR_NAME}'.'${timeStampStr}
+
+if [ -f "${WORKBENCH_DEPLOY_DIR}/${APP_JAR_NAME}" ]; then
+    mv ${WORKBENCH_DEPLOY_DIR}/${APP_JAR_NAME} ${APP_JAR_BAK}
+fi
+
+cp ${APP_JAR_DIR}/${APP_JAR_NAME} ${WORKBENCH_DEPLOY_DIR}/
+
+echo restarted ${APP_JAR_DIR}/${APP_JAR_NAME}
+# end
