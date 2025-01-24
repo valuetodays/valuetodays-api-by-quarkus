59864fb3f4bac8a34b2291a62b5133f534b40192
valuetodays <abcdefg20111111@gmail.com>
2025-01-24 22:51:20 +0800

    feat: upload file

--- src/main/java/cn/valuetodays/demo/controller/DockerStatsController.java
+++ src/main/java/cn/valuetodays/demo/controller/DockerStatsController.java
@@ -2,20 +2,30 @@ package cn.valuetodays.demo.controller;
 
 import cn.valuetodays.demo.persist.DockerStatsPersist;
 import cn.valuetodays.demo.service.DockerStatsService;
-import cn.valuetodays.demo.vo.SimpleTypeReq;
 import cn.valuetodays.demo.vo.save.DockerStatsReq;
 import cn.vt.util.JsonUtils;
 import com.fasterxml.jackson.core.type.TypeReference;
 import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.MediaType;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.eclipse.microprofile.context.ManagedExecutor;
+import org.jboss.resteasy.reactive.server.multipart.FileItem;
+import org.jboss.resteasy.reactive.server.multipart.FormValue;
+import org.jboss.resteasy.reactive.server.multipart.MultipartFormDataInput;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 /**
  * .
@@ -24,6 +34,7 @@ import java.util.Objects;
  * @since 2025-01-24
  */
 @Path("/dockerStats")
+@Slf4j
 public class DockerStatsController {
     @Inject
     DockerStatsService dockerStatsService;
@@ -39,24 +50,38 @@ public class DockerStatsController {
 
     @POST
     @Path("saveByText")
-    public Map<String, Object> saveByText(SimpleTypeReq req) {
-        if (Objects.isNull(req)) {
-            return Map.of();
-        }
-        String text = req.getText();
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Map<String, Object> saveByText(MultipartFormDataInput dataInput) {
+        String text = getFileContentAsString(dataInput, "file", StandardCharsets.US_ASCII);
         if (StringUtils.isBlank(text)) {
             return Map.of();
         }
+
         managedExecutor.execute(() -> {
-            String json = "[" + text + "{}]";
+            String json = "[" + StringUtils.substringBeforeLast(text, ",") + "]";
             List<DockerStatsReq> dockerStatsReqs = JsonUtils.fromJson(json, new TypeReference<List<DockerStatsReq>>() {
             });
             for (DockerStatsReq obj : dockerStatsReqs) {
-                if (StringUtils.isNotBlank(obj.getName())) {
-                    dockerStatsService.save(obj.toPersist());
-                }
+                dockerStatsService.save(obj.toPersist());
             }
         });
         return Map.of("data", 1, "code", 0, "time", System.currentTimeMillis()); // <4>
+    }
+
+    private String getFileContentAsString(MultipartFormDataInput dataInput, String fileId, Charset charset) {
+        Map<String, Collection<FormValue>> values = dataInput.getValues();
+        if (MapUtils.isEmpty(values)) {
+            return null;
+        }
+        Collection<FormValue> fileCollection = values.get(fileId);
+        FormValue[] fileFormValue = fileCollection.toArray(FormValue[]::new);
+        FormValue formValue = fileFormValue[0];
+        FileItem fileItem = formValue.getFileItem();
+        java.nio.file.Path file = fileItem.getFile();
+        try {
+            return FileUtils.readFileToString(file.toFile(), charset);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
