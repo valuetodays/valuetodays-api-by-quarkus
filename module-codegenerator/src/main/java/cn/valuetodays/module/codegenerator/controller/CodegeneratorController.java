875054c800d7651477ffc13dc7200b1a71ce64e8
valuetodays <abcdefg20111111@gmail.com>
2025-06-01 22:49:37 +0800

    opt: response as json

--- module-codegenerator/src/main/java/cn/valuetodays/module/codegenerator/controller/CodegeneratorController.java
+++ module-codegenerator/src/main/java/cn/valuetodays/module/codegenerator/controller/CodegeneratorController.java
@@ -19,8 +19,11 @@ import cn.vt.exception.AssertUtils;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 import jakarta.validation.Valid;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Collection;
@@ -38,6 +41,8 @@ import java.util.stream.Stream;
 @Slf4j
 @RequestScoped
 @Path("/cg/codegenerator")
+@Produces({MediaType.APPLICATION_JSON})
+@Consumes({MediaType.APPLICATION_JSON})
 public class CodegeneratorController {
 
     @Inject
