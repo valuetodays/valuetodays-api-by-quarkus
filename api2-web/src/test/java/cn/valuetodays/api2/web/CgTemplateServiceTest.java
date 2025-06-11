d23ee46d4a268b006429a4b199e4e7b2bca6ae0e
valuetodays <abcdefg20111111@gmail.com>
2025-06-11 22:00:16 +0800

    feat: test QuerySearch#like

--- api2-web/src/test/java/cn/valuetodays/api2/web/CgTemplateServiceTest.java
+++ api2-web/src/test/java/cn/valuetodays/api2/web/CgTemplateServiceTest.java
@@ -3,7 +3,9 @@ package cn.valuetodays.api2.web;
 import cn.valuetodays.module.codegenerator.dao.CgTemplateDAO;
 import cn.valuetodays.module.codegenerator.po.CgTemplatePO;
 import cn.valuetodays.module.codegenerator.service.CgTemplateService;
+import cn.valuetodays.quarkus.commons.base.Operator;
 import cn.valuetodays.quarkus.commons.base.PageQueryReqIO;
+import cn.valuetodays.quarkus.commons.base.QuerySearch;
 import cn.vt.web.RestPageImpl;
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.inject.Inject;
@@ -11,6 +13,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.hamcrest.CoreMatchers;
 import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 /**
  * Tests for {@link CgTemplateDAO}.
@@ -29,6 +33,10 @@ public class CgTemplateServiceTest {
         PageQueryReqIO req = new PageQueryReqIO();
         req.setPageNum(1);
         req.setPageSize(10);
+        List<QuerySearch> searches = List.of(
+            QuerySearch.of("title", "quarkus", Operator.LIKE)
+        );
+        req.setSearches(searches);
         RestPageImpl<CgTemplatePO> query = cgTemplateService.query(req);
         log.info("query={}", query);
         MatcherAssert.assertThat(query, CoreMatchers.notNullValue());
