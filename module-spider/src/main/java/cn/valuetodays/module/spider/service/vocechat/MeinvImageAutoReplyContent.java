8cb6988df21f88c566f98541723675b9b78736c9
valuetodays <abcdefg20111111@gmail.com>
2025-06-16 21:25:19 +0800

    webp to jpg

--- module-spider/src/main/java/cn/valuetodays/module/spider/service/vocechat/MeinvImageAutoReplyContent.java
+++ module-spider/src/main/java/cn/valuetodays/module/spider/service/vocechat/MeinvImageAutoReplyContent.java
@@ -1,10 +1,13 @@
 package cn.valuetodays.module.spider.service.vocechat;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 import cn.valuetodays.api2.web.basic.push.vocechat.AutoReplyContent;
 import cn.valuetodays.api2.web.basic.push.vocechat.PushBaseReq;
+import cn.valuetodays.api2.web.common.WebpToJpgUtils;
+import cn.vt.exception.CommonException;
 import jakarta.enterprise.context.ApplicationScoped;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.tuple.Pair;
@@ -29,6 +32,11 @@ public class MeinvImageAutoReplyContent implements AutoReplyContent {
             return null;
         }
         File first = files.getFirst();
-        return AutoReplyContent.makeFile(first.getAbsolutePath());
+        try {
+            File file = WebpToJpgUtils.webpToJpg(first);
+            return AutoReplyContent.makeFile(file.getAbsolutePath());
+        } catch (IOException e) {
+            throw new CommonException(e);
+        }
     }
 }
