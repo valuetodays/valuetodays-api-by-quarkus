3cea760587f83a6f0bc3eebbeeaecf735520c5dc
valuetodays <abcdefg20111111@gmail.com>
2025-06-19 22:29:53 +0800

    add queryRunner

--- module-fortune/src/main/java/cn/valuetodays/api2/module/fortune/controller/StockHoldersController.java
+++ module-fortune/src/main/java/cn/valuetodays/api2/module/fortune/controller/StockHoldersController.java
@@ -42,6 +42,7 @@ public class StockHoldersController
     @Path(value = "/getAccountProfit")
     @POST
     public List<AccountProfitVo> getAccountProfit(GetAccountProfitReq req) {
+        super.fillLoginAccount(req);
         return getService().getAccountProfit(req);
     }
 
